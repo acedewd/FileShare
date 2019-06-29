33c3d4ddcce758ebc0d7217552b2a860a0bce89b
Alex Quartulli <alex.m.quartulli@gmail.com>
2019-06-28 22:27:15 -0400

    Create solution

---
+++ FileShare/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FileShare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
